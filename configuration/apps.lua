4313ec8164164826326b75ee0b31f2930d92eea9
HikariKnight <2557889+HikariKnight@users.noreply.github.com>
2019-09-10 18:53:51 +0200

    add startup applications and dpi aware rofi * rofi is not dpi aware * added startup programs i use

--- configuration/apps.lua
+++ configuration/apps.lua
@@ -1,19 +1,44 @@
 local filesystem = require('gears.filesystem')
 
+-- Thanks to jo148 on github for making rofi dpi aware!
+local with_dpi = require('beautiful').xresources.apply_dpi
+local get_dpi = require('beautiful').xresources.get_dpi
+local rofi_command = 'rofi -dpi ' .. get_dpi() .. ' -width ' .. with_dpi(400) .. ' -show drun -theme ' .. filesystem.get_configuration_dir() .. '/configuration/rofi.rasi'
+
 return {
   -- List of apps to start by default on some actions
   default = {
-    terminal = 'alacritty',
-    editor = 'code',
-    rofi = 'rofi -show drun -theme ' .. filesystem.get_configuration_dir() .. '/configuration/rofi.rasi',
-    lock = 'i3lock-fancy-rapid 5 3 -k --timecolor=ffffffff --datecolor=ffffffff',
-    quake = 'alacritty --title QuakeTerminal'
+    terminal = 'konsole',
+    rofi = rofi_command,
+    lock = 'i3lock-fancy',
+    quake = 'konsole',
+    screenshot = '~/.config/awesome/configuration/utils/screenshot -m',
+    region_screenshot = '~/.config/awesome/configuration/utils/screenshot -r',
+    delayed_screenshot = 'sleep 10 ; ~/.config/awesome/configuration/utils/screenshot -r',
+    
+    -- Editing these also edits the default program
+    -- associated with each tag/workspace
+    browser = 'firefox',
+    editor = 'code', -- gui text editor
+    social = 'discord',
+    game = rofi_command,
+    files = 'nautilus',
+    music = rofi_command
   },
   -- List of apps to start once on start-up
   run_on_start_up = {
     'compton --config ' .. filesystem.get_configuration_dir() .. '/configuration/compton.conf',
-    'blueberry-tray', -- Bluetooth tray icon
-    'xfce4-power-manager', -- Power manager
-    '/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 & eval $(gnome-keyring-daemon -s --components=pkcs11,secrets,ssh,gpg)' -- credential manager
+    'nm-applet --indicator', -- wifi
+    --'blueberry-tray', -- Bluetooth tray icon
+    --'xfce4-power-manager', -- Power manager
+    'ibus-daemon --xim', -- Ibus daemon for keyboard
+    'scream -u -p 4011 -i virbr1', -- scream audio sink
+    'numlockx on', -- enable numlock
+    '/usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 & eval $(gnome-keyring-daemon -s --components=pkcs11,secrets,ssh,gpg)', -- credential manager
+    --'/usr/lib/x86_64-linux-gnu/libexec/polkit-kde-authentication-agent-1 & eval $(gnome-keyring-daemon -s --components=pkcs11,secrets,ssh,gpg)', -- credential manager
+
+    -- Add applications that need to be killed between reloads
+    -- to avoid multipled instances, inside the awspawn script
+    '~/.config/awesome/configuration/awspawn' -- Spawn "dirty" apps that can linger between sessions
   }
 }
