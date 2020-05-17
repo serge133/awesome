c30fe46f7fd26a5f05b7a6dc7674a1aa5db807f3
HikariKnight <2557889+HikariKnight@users.noreply.github.com>
2020-05-17 21:16:19 +0200

    update to use scream ivshmem

--- configuration/apps.lua
+++ configuration/apps.lua
@@ -32,7 +32,7 @@ return {
     --'blueberry-tray', -- Bluetooth tray icon
     --'xfce4-power-manager', -- Power manager
     'ibus-daemon --xim --daemonize', -- Ibus daemon for keyboard
-    'scream -u -p 4011 -i virbr1', -- scream audio sink
+    'scream-start', -- scream audio sink
     'numlockx on', -- enable numlock
     '/usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 & eval $(gnome-keyring-daemon -s --components=pkcs11,secrets,ssh,gpg)', -- credential manager
     --'/usr/lib/x86_64-linux-gnu/libexec/polkit-kde-authentication-agent-1 & eval $(gnome-keyring-daemon -s --components=pkcs11,secrets,ssh,gpg)', -- credential manager
