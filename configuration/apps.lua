92c9114fa29414970c9e649ac87c844a81aba321
HikariKnight <2557889+HikariKnight@users.noreply.github.com>
2020-05-24 16:45:34 +0200

    switch my config to not rely on konsole anymore

--- configuration/apps.lua
+++ configuration/apps.lua
@@ -8,10 +8,10 @@ local rofi_command = 'env /usr/bin/rofi -dpi ' .. get_dpi() .. ' -width ' .. wit
 return {
   -- List of apps to start by default on some actions
   default = {
-    terminal = 'konsole',
+    terminal = 'gnome-terminal',
     rofi = rofi_command,
     lock = 'i3lock-fancy',
-    quake = 'konsole',
+    quake = 'gnome-terminal',
     screenshot = '~/.config/awesome/configuration/utils/screenshot -m',
     region_screenshot = '~/.config/awesome/configuration/utils/screenshot -r',
     delayed_screenshot = 'sleep 10 ; ~/.config/awesome/configuration/utils/screenshot -r',
